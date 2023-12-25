d77ee827d0ddf0b137e16a6b1aaa2cd5ba13dfdf
bhowmickkrishnendu <krishnendu2u@gmail.com>
2023-12-25 20:04:44 +0530

    docs: Add comments for region and availability zone variables
    - Comment added for the 'region' variable, specifying it represents the Mumbai region
    - Comment added for the 'availability_zone' variable, detailing it represents Mumbai region, Availability Zone 'a'
    - Added a new variable 'availability_zone1' representing Mumbai region, Availability Zone 'b' with appropriate comment
    
    # Include any additional context or details as needed

--- VPC_Creation/vat.tf
+++ VPC_Creation/vat.tf
@@ -5,8 +5,13 @@ variable "region" {
 }
 
 # Define the variable for the AWS availability zone within the Mumbai region
-
 variable "availability_zone" {
     description = "This is mumbai region a zone"
     default = "ap-south-1a"
+}
+
+# Define an additional variable for another AWS availability zone within the Mumbai regio
+variable "availability_zone1" {
+    description = "This is mumbai region b zone"
+    default = "ap-south-1b"
 }
