c2b6ae6f6b0d9c779e74e28035432d306a23612f
bhowmickkrishnendu <krishnendu2u@gmail.com>
2023-12-27 08:12:14 +0530

    feat: Introduce AWS key pair for Mumbai EC2 instance

--- Single_EC2_Create/main.tf
+++ Single_EC2_Create/main.tf
@@ -4,6 +4,25 @@ provider "aws" {
     alias = "main"
 }
 
+# AWS key pair resource for secure SSH access
+resource "aws_key_pair" "Mumbai_First_Server" {
+    key_name = "Mumbai_First_Server"
+    public_key = tls_private_key.rsa.public_key_openssh
+}
+
+# TLS private key resource for the AWS key pair
+resource "tls_private_key" "rsa" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+# Local file resource to save the private key locally
+resource "local_file" "Mumbai_Key" {
+    content = tls_private_key.rsa.private_key_pem
+    filename = "Mumbai_First_Server"
+}
+
+
 # AWS EC2 instance resource
 resource "aws_instance" "Mumbai_First_Server" {
 
@@ -13,6 +32,7 @@ resource "aws_instance" "Mumbai_First_Server" {
     vpc_security_group_ids = [ "${aws_security.allow_ssh.id}" ]     # Specify the security group ID
     associate_public_ip_address = true                              # Assign a public IP address to the instance
     disable_api_termination = true                                  # Disable API termination for the instance
+    key_name = "Mumbai_First_Server"                                # Key name mention here
 
     # Root block device configuration
     root_block_device {
