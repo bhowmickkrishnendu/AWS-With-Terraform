b7b32deb30dda6a0324017eaacb5df7d211b3bf2
bhowmickkrishnendu <krishnendu2u@gmail.com>
2023-12-31 09:26:41 +0530

    docs: Add descriptive comments for variables in var.tf
    - Provide detailed comments for each variable in var.tf to enhance understanding
    - Clarify the purpose and default values of variables related to the AWS region and S3 bucket folder structure

--- S3_Bucket_Private_With_Policy_Versioning/var.tf
+++ S3_Bucket_Private_With_Policy_Versioning/var.tf
@@ -1,5 +1,23 @@
 # Define the variable for the AWS region (Mumbai)
 variable "region" {
-  description = "This is Mumbai region s3 buck"  
-  default = "ap-south-1"
+  description = "This is the AWS region for the S3 bucket in Mumbai"
+  default     = "ap-south-1"
+}
+
+# Define the variable for the first folder
+variable "first_folder" {
+  description = "This is the first folder in the S3 bucket"
+  default     = "folder_sun/"
+}
+
+# Define the variable for the second folder
+variable "second_folder" {
+  description = "This is the second folder in the S3 bucket"
+  default     = "folder_moon/"
+}
+
+# Define the variable for the sub-folder of the second folder
+variable "sub_folder_of_second" {
+  description = "This is the sub-folder of the second folder in the S3 bucket"
+  default     = "folder_moon/folder_galaxy/"
 }
