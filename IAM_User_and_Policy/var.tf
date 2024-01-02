9a2d2cbbfaa39131f0cd8c863116dabfe542b76e
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-02 08:42:16 +0530

    docs: Add descriptions to variables in var.tf for better documentation

--- IAM_User_and_Policy/var.tf
+++ IAM_User_and_Policy/var.tf
@@ -1,6 +1,10 @@
 # Define the variable for the AWS region (Mumbai)
 variable "region" {
-    description = "This AWS mumbai region"
-    default = "ap-south-1"
-  
+    description = "This AWS Mumbai region"
+    default     = "ap-south-1"
+}
+
+# Define the default policy name for IAM user
+variable "user_policy_name" {
+    default = "Peter_NY_Policy"
 }
