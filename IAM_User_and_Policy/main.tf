6c58a7046a6b3f0384e63aa316d57272c60eda8f
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-02 08:43:49 +0530

    feat: Add AWS IAM policy and attachment configuration
    - Define an IAM policy named "Peter_NY_Policy" with EC2 Describe permissions.
    - Attach the IAM policy to the IAM user "Peter_NY."

--- IAM_User_and_Policy/main.tf
+++ IAM_User_and_Policy/main.tf
@@ -20,6 +20,32 @@ resource "aws_iam_access_key" "keyname" {
     pgp_key = file("publicbase64.key")  # Specify the PGP key for encrypting the secret
 }
 
+# AWS IAM policy resource creation
+resource "aws_iam_policy" "policy_details" {
+  name        = var.user_policy_name  # Specify the name of the IAM policy
+  path        = "/"                   # Set the path for the IAM policy
+  description = "This is the user policy"  # Add a description for the IAM policy
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            Action = [
+                "ec2:Describe*",
+            ]
+            Effect   = "Allow"
+            Resource = "*"
+        },
+    ]
+  })
+}
+
+# AWS IAM user policy attachment
+resource "aws_iam_user_policy_attachment" "policy_attach" {
+  user        = aws_iam_user.username.name       # Reference the IAM user
+  policy_arn  = aws_iam_policy.policy_details.arn  # Reference the IAM policy ARN
+}
+
 # Define Terraform Output to expose the encrypted secret and access key of the IAM access key
 output "encrypted_secret_key" {
     value = aws_iam_access_key.keyname.encrypted_secret  # Expose the encrypted secret of the IAM access key
