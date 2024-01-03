57e888aad7774c88a6c8b84072e1db1576fae7dd
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-03 06:54:03 +0530

    Add AWS region variable with description for Mumbai (ap-south-1).

---
+++ IAM_Role_and_Policy/var.tf
@@ -0,0 +1,5 @@
+# Define the variable for the AWS region (Mumbai)
+variable "region" {
+    description = "This variable specifies the AWS Mumbai region"
+    default     = "ap-south-1"  # Set the default AWS region to Mumbai (ap-south-1)
+}
