37a4b64f1402ab06e34ba12bcdf3790f8c107139
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-03 07:45:33 +0530

    Add AWS provider configuration, Amazon EKS VPC creation, IAM role, and policies for EKS cluster and worker nodes.

--- EKS_Cluster/main.tf
+++ EKS_Cluster/main.tf
@@ -1,17 +1,18 @@
 # Define the AWS provider
 provider "aws" {
-  region = var.region
+  region = var.region  # Set the AWS region based on the 'region' variable
 }
 
 # Create the Amazon VPC using a CloudFormation template
 resource "aws_cloudformation_stack" "eks_vpc_stack" {
-  name         = "EKS-VPC-Private"
-  template_url = "http://amazon-eks.s3.us-west-2.amazonaws.com/cloudformation/2020-08-12/amazon-eks-vpc-private-subnets.yaml"
+  name         = "EKS-VPC-Private"  # Specify a name for the CloudFormation stack
+  template_url = "http://amazon-eks.s3.us-west-2.amazonaws.com/cloudformation/2020-08-12/amazon-eks-vpc-private-subnets.yaml"  # Use the provided CloudFormation template URL
 }
 
-# Create the IAM role
+# Create the IAM role for the Amazon EKS cluster
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "EKS_ClusterRole"
+  name = "EKS_ClusterRole"  # Specify the name for the IAM role
+
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
@@ -26,13 +27,14 @@ resource "aws_iam_role" "eks_cluster_role" {
 
 # Attach the Amazon EKS cluster policy to the IAM role
 resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"  # Attach the Amazon EKS cluster policy
+  role       = aws_iam_role.eks_cluster_role.name  # Reference the created IAM role
 }
 
-# Create the IAM role for node worker group
+# Create the IAM role for the Amazon EKS node worker group
 resource "aws_iam_role" "eks_worker_node_role" {
-  name = "EKS_WorkerNodeRole"
+  name = "EKS_WorkerNodeRole"  # Specify the name for the IAM role
+
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
@@ -45,18 +47,18 @@ resource "aws_iam_role" "eks_worker_node_role" {
   })
 }
 
-# Attach policies to the IAM role for node worker group
+# Attach policies to the IAM role for the Amazon EKS node worker group
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment_1" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"  # Attach the Amazon EKS worker node policy
+  role       = aws_iam_role.eks_worker_node_role.name  # Reference the created IAM role
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment_2" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # Attach the Amazon EKS CNI policy
+  role       = aws_iam_role.eks_worker_node_role.name  # Reference the created IAM role
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment_3" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_worker_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"  # Attach the Amazon ECR read-only policy
+  role       = aws_iam_role.eks_worker_node_role.name  # Reference the created IAM role
 }
