e6d52d4a0d54e4279f51f6e900945ac8fb818773
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-11 21:39:09 +0530

    🔒 Introduce AWS security group and EC2 instance resources for Mumbai_First_Server

--- EC2_Create_with_EIP/main.tf
+++ EC2_Create_with_EIP/main.tf
@@ -21,3 +21,61 @@ resource "local_file" "Mumbai_Key" {
     content = tls_private_key.rsa.private_key_pem
     filename = "Mumbai_First_Server.pem"
 }
+
+# AWS security group resource to allow SSH traffic
+resource "aws_security_group" "allow_ssh" {
+    name        = "Mumbai_First_Server-SG"
+    description = "Control Traffic"
+    vpc_id      = "vpc-0e5c7dd3912759f92"                                # Specify the VPC ID
+
+    # Ingress rule to allow SSH traffic
+    ingress {
+        description = "Allow SSH traffic"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Egress rule to allow traffic outside
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Tags for the security group
+    tags = {
+        Name = "Mumbai_First_Server-SG"
+    }
+}
+
+# AWS EC2 instance resource
+resource "aws_instance" "Mumbai_First_Server" {
+    ami                      = "ami-0a7cf821b91bcccbc"             # Specify the Amazon Machine Image (AMI)
+    instance_type            = "t2.micro"                            # Specify the instance type
+    subnet_id                = "subnet-05160f5164e900466"          # Specify the subnet ID
+    vpc_security_group_ids   = [aws_security_group.allow_ssh.id]   # Specify the security group ID
+    associate_public_ip_address = true                              # Assign a public IP address to the instance
+    disable_api_termination  = true                                 # Disable API termination for the instance
+    key_name                 = "Mumbai_First_Server"               # Key name mention here
+
+    # Root block device configuration
+    root_block_device {
+        volume_size           = 10
+        volume_type           = "gp2"
+        delete_on_termination = false
+    }
+
+    # Tags for the EC2 instance
+    tags = {
+        Name = "Mumbai_First_server"
+    }
+
+    # Tags for the attached volume
+    volume_tags = {
+        Name = "Mumbai_First_Server"
+    }
+}
+
