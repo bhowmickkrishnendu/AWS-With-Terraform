ee9c62b3c6ef3c843968b3536b4b713713c0cb0e
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-12 22:13:38 +0530

    🚀 Introduce Terraform code for provisioning an EC2 instance with an associated Elastic IP!
    🔐 Create AWS resources such as a key pair, security group, and root block device configuration for secure SSH access.
    🌐 Allocate and associate an Elastic IP with the EC2 instance for a static public IP address.
    🔍 Define output variables to expose essential information, including private IP, EC2 instance ID, and Elastic IP.
    🛡️ Ensure proper resource dependencies and configurations for a robust and functional setup.
    
    #terraform #aws #ec2 #elasticip 🛠️

--- EC2_Create_with_EIP/main.tf
+++ EC2_Create_with_EIP/main.tf
@@ -1,81 +1,99 @@
 # AWS provider configuration
 provider "aws" {
-  alias = "main"
+  alias  = "main"
   region = var.region
 }
 
 # AWS key pair resource for secure SSH access
 resource "aws_key_pair" "Mumbai_First_Server" {
-    key_name = "Mumbai_First_Server"
-    public_key = tls_private_key.rsa.public_key_openssh
+  key_name   = "Mumbai_First_Server"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 # TLS private key resource for the AWS key pair
 resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 # Local file resource to save the private key locally
 resource "local_file" "Mumbai_Key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = "Mumbai_First_Server.pem"
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "Mumbai_First_Server.pem"
 }
 
 # AWS security group resource to allow SSH traffic
 resource "aws_security_group" "allow_ssh" {
-    name        = "Mumbai_First_Server-SG"
-    description = "Control Traffic"
-    vpc_id      = "vpc-0e5c7dd3912759f92"                                # Specify the VPC ID
+  name        = "Mumbai_First_Server-SG"
+  description = "Control Traffic"
+  vpc_id      = "vpc-0e5c7dd3912759f92"  # Specify the VPC ID
 
-    # Ingress rule to allow SSH traffic
-    ingress {
-        description = "Allow SSH traffic"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Ingress rule to allow SSH traffic
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Egress rule to allow traffic outside
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Egress rule to allow traffic outside
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Tags for the security group
-    tags = {
-        Name = "Mumbai_First_Server-SG"
-    }
+  # Tags for the security group
+  tags = {
+    Name = "Mumbai_First_Server-SG"
+  }
 }
 
-# AWS EC2 instance resource
-resource "aws_instance" "Mumbai_First_Server" {
-    ami                      = "ami-0a7cf821b91bcccbc"             # Specify the Amazon Machine Image (AMI)
-    instance_type            = "t2.micro"                            # Specify the instance type
-    subnet_id                = "subnet-05160f5164e900466"          # Specify the subnet ID
-    vpc_security_group_ids   = [aws_security_group.allow_ssh.id]   # Specify the security group ID
-    associate_public_ip_address = true                              # Assign a public IP address to the instance
-    disable_api_termination  = true                                 # Disable API termination for the instance
-    key_name                 = "Mumbai_First_Server"               # Key name mention here
+# AWS Elastic IP resource
+resource "aws_instance" "elastic_ip" {
+  ami                      = "ami-0a7cf821b91bcccbc"
+  instance_type            = "t2.micro"
+  subnet_id                = "subnet-05160f5164e900466"
+  vpc_security_group_ids   = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+  disable_api_termination  = true
+  key_name                 = "Mumbai_First_Server"
 
-    # Root block device configuration
-    root_block_device {
-        volume_size           = 10
-        volume_type           = "gp2"
-        delete_on_termination = false
-    }
-
-    # Tags for the EC2 instance
+  # Root block device configuration
+  root_block_device {
+    volume_size           = 10
+    volume_type           = "gp2"
+    delete_on_termination = false
     tags = {
-        Name = "Mumbai_First_server"
+      Name = "Mumbai_First_server_with_elastic_ip"
     }
+  }
 
-    # Tags for the attached volume
-    volume_tags = {
-        Name = "Mumbai_First_Server"
-    }
+  # Tags for the EC2 instance
+  tags = {
+    Name = "Mumbai_First_server_with_elastic_ip"
+  }
 }
 
+resource "aws_eip" "instance" {
+  instance = aws_instance.elastic_ip.id
+  domain   = "vpc"
+  tags = {
+    Name   = "Mumbai_First_server_with_elastic_ip"
+  }
+}
+
+# Output block to expose relevant information
+output "private_key" {
+  value = aws_instance.elastic_ip.private_ip
+}
+
+output "ec2_id" {
+  value = aws_instance.elastic_ip.id
+}
+
+output "elastic_ip" {
+  value = aws_eip.instance.public_ip
+}
