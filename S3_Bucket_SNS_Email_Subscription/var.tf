27185bd8c02827b347c5a8b342d434edb2a7b1f0
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-01-23 17:57:30 +0530

    🔧 Add variable descriptions and defaults
    🌐 `region` variable now has a description indicating it represents the Mumbai region.
    📦 Added comments for better understanding of `bucketname` and `topicname` variables with their default values.
    👌 Improved code documentation for variable definitions.

--- S3_Bucket_SNS_Email_Subscription/var.tf
+++ S3_Bucket_SNS_Email_Subscription/var.tf
@@ -1,13 +1,15 @@
 # Define the variable for the AWS region (Mumbai)
 variable "region" {
-  description = "This is the Mumbai region"
-  default     = "ap-south-1"
+  description = "This is the Mumbai region"  # Description of the variable, indicating it's for the Mumbai region
+  default     = "ap-south-1"                 # Default value set to the AWS Mumbai region code
 }
 
+# Define variable for S3 bucket name
 variable "bucketname" {
-  default = "krish-s3-demo"
+  default = "krish-s3-demo"  # Default value set to the desired S3 bucket name
 }
 
+# Define variable for SNS topic name
 variable "topicname" {
-  default = "krish-s3-demo"
+  default = "krish-s3-demo"  # Default value set to the desired SNS topic name
 }
