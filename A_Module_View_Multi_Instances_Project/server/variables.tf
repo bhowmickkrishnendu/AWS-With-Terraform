d31f3f20f9d93ccf851ddc5e2441c54b1c2b40cf
bhowmickkrishnendu <krishnendu2u@gmail.com>
2024-03-30 06:47:27 +0530

    🆕Network details added on var files.

--- A_Module_View_Multi_Instances_Project/server/variables.tf
+++ A_Module_View_Multi_Instances_Project/server/variables.tf
@@ -5,3 +5,19 @@ variable "region" {
 variable "instance_name" {
   default = "java_app_server"
 }
+
+variable "amazon_linux_2023_ami" {
+  default = "ami-0ec0e125bb6c6e8ec"
+}
+
+variable "vpc_id" {
+  default = "vpc-0243e953b038c0d1f"
+}
+
+variable "public_subnet_id" {
+  default = "vpc-0243e953b038c0d1f"
+}
+
+variable "t2.micro" {
+  default = "t2.micro"
+}
