8b5f7d0c0948eafc3b7d665ef263e22c4169ba3c
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-01-03 22:26:51 +0530

    🆕 add subnet for VPC

---
+++ v2_2025/module/AWS-VPC-Module/subnet.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.availability_zone1
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-subnet"
+  }
+}
