0e68747365c250162bea4e5bcac739e1bb7716da
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-01-04 22:27:29 +0530

    🆕 add route table for VPC

---
+++ v2_2025/module/AWS-VPC-Module/route_table.tf
@@ -0,0 +1,34 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = var.enable_nat_gateway ? aws_nat_gateway.nat[0].id : aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
