475eeaa52eee0bfc02d991ad37bda2c20883b2b8
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-01-16 09:54:00 +0530

    commit: v2_2025/AWS-Main/IAM_Role_Policy_Root/main.tf, v2_2025/AWS-Main/IAM_Role_Policy_Root/_variables.tf changed on master at 2025-01-16 09:54

---
+++ v2_2025/AWS-Main/IAM_Role_Policy_Root/_variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "This is  the AWS region to create resources"
+  type = string
+}
+
+variable "role_name" {
+  description = "The name of the IAM role to create."
+  type = string
+}
+
+variable "user_policy_name" {
+  
+}
--- v2_2025/AWS-Main/IAM_Role_Policy_Root/main.tf
+++ v2_2025/AWS-Main/IAM_Role_Policy_Root/main.tf
@@ -1,3 +1,7 @@
 module "iam_role_policy" {
-  source = "D://"
+  source = "D:\\my_github\\AWS-With-Terraform\\v2_2025\\module\\IAM_Role_Policy_Module"
+  region = var.region
+  role_name = var.role_name
+  user_policy_name = var.user_policy_name
+
 }
