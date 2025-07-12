088b67e0cbb917006831e8dd6895c95ae07b6634
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-07-12 20:19:51 +0530

    feat: moved IAM module to separate repo https://github.com/bhowmickkrishnendu/terraform-aws-iam-role-policy

--- v2_2025/AWS-Main/IAM_Role_Policy_Root/main.tf
+++ v2_2025/AWS-Main/IAM_Role_Policy_Root/main.tf
@@ -1,5 +1,6 @@
 module "iam_role_policy" {
-  source = "D:\\my_github\\AWS-With-Terraform\\v2_2025\\module\\IAM_Role_Policy_Module"
+  # source = "D:\\my_github\\AWS-With-Terraform\\v2_2025\\module\\IAM_Role_Policy_Module"
+  source = "git::"
   region = var.region
   role_name = var.role_name
   user_policy_name = var.user_policy_name
--- v2_2025/module/IAM_Role_Policy_Module/provider.tf
+++
@@ -1,6 +0,0 @@
-# AWS Provider configuration
-provider "aws" {
-    alias  = "main"
-    region = var.region  # Set the AWS region based on the 'region' variable  
-}
-
