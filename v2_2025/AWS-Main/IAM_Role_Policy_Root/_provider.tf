15b11b2f1e3ab71afb38426722932d16db8a548d
bhowmickkrishnendu <krishnendu2u@gmail.com>
2025-07-14 10:21:39 +0530

    Provider updated.

--- v2_2025/AWS-Main/IAM_Role_Policy_Root/_provider.tf
+++ v2_2025/AWS-Main/IAM_Role_Policy_Root/_provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
   backend "s3" {
     bucket         = "krishnendu-tf-state-bucket"
@@ -6,5 +12,11 @@ terraform {
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-lock-table"
+    
   }
 }
+
+provider "aws" {
+  region = var.region
+}
+
