4f61afa5e5742636f6ecf37e5dfa97c6e2c61b87
bhowmickkrishnendu <krishnendu2u@gmail.com>
2026-05-15 08:58:52 +0530

    Remove AWS profile dependency for CI compatibility

--- environments/dev/networking/backend.tf
+++ environments/dev/networking/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "krish-terraform-state-ap-south-1"
     key          = "dev/networking/terraform.tfstate"
     region       = "ap-south-1"
-    profile      = "terraform-dev"
+    # profile      = "terraform-dev"
     encrypt      = true
     use_lockfile = true
   }
--- environments/dev/networking/variables.tf
+++ environments/dev/networking/variables.tf
@@ -6,9 +6,9 @@ variable "aws_region" {
   type = string
 }
 
-variable "aws_profile" {
-  type = string
-}
+# variable "aws_profile" {
+#   type = string
+# }
 
 variable "vpc_cidr" {
   type = string
