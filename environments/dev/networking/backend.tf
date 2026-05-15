7333a02282526acf69a3bfdc23c921fa47cfad19
bhowmickkrishnendu <krishnendu2u@gmail.com>
2026-05-15 09:56:30 +0530

    fix: tf fmt.

--- environments/dev/networking/backend.tf
+++ environments/dev/networking/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "krish-terraform-state-ap-south-1"
-    key          = "dev/networking/terraform.tfstate"
-    region       = "ap-south-1"
+    bucket = "krish-terraform-state-ap-south-1"
+    key    = "dev/networking/terraform.tfstate"
+    region = "ap-south-1"
     # profile      = "terraform-dev"
     encrypt      = true
     use_lockfile = true
